9fdab237d1624e844dcddc63355cfe7e9d206a7b
X-creator <farengeyt12@mail.ru>
2021-05-20 21:57:07 +0500

    fix: missing semicolon in app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const user = require('./controllers/usercontroller');
-const game = require('./controllers/gamecontroller')
+const game = require('./controllers/gamecontroller');
 
 
 db.sync();
 app.use(require('body-parser'));
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
+app.use(require('./middleware/validate-session'));
 app.use('/api/game', game);
 app.listen(function() {
     console.log("App is listening on 4000");
-})
+});
