1f3f65111b7794d543bbd77184db802aeabc319b
X-creator <farengeyt12@mail.ru>
2021-05-20 22:48:39 +0500

    fix: missing body-parser json method in db.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ db.sync();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(bodyParser.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'));
 app.use('/api/game', game);
