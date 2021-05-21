e2063d0fb601b3d977b22f7c13130c3f10f239fb
X-creator <farengeyt12@mail.ru>
2021-05-21 15:38:04 +0500

    refactor: consistent require in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./db');
 const user = require('./controllers/usercontroller');
 const game = require('./controllers/gamecontroller');
 const bodyParser = require('body-parser');
+const validateSession = require('./middleware/validate-session');
 
 
 db.sync();
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'));
+app.use(validateSession);
 app.use('/api/game', game);
 app.listen(process.env.PORT,function() {
     console.log("App is listening on 4000");
